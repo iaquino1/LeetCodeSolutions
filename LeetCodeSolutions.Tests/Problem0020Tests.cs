e683d9e564f81c588064fdd5652f8afe3ed5899c
Luis Enrique Aquino Cortés <aquino.lec@gmail.com>
2025-01-25 13:21:13 -0600

    Problema 20 Valid Parentheses.
    Se agrega solución y prueba unitaria del problema 20 Valid Parentheses.

---
+++ LeetCodeSolutions.Tests/Problem0020Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeSolutions.Problems.Problem0020_ValidParentheses;
+using NUnit.Framework;
+
+namespace LeetCodeSolutions.Tests;
+
+[TestFixture]
+public class Problem0020Tests
+{
+    private static IEnumerable<TestCaseData> ValidParenthesesTestCases
+    {
+        get
+        {
+            yield return new TestCaseData("()", true);
+            yield return new TestCaseData("()[]{}", true);
+            yield return new TestCaseData("(]", false);
+            yield return new TestCaseData("([)]", false);
+            yield return new TestCaseData("{[]}", true);
+        }
+    }
+
+    [Test, TestCaseSource(nameof(ValidParenthesesTestCases))]
+    public void IsValidParentheses_ShouldReturnExpectedResult(string input, bool expected)
+    {
+        // Act
+        bool result = Problem0020_ValidParentheses.IsValid(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ LeetCodeSolutions/Problems/Problem0020_ValidParentheses/Problem0020_ValidParentheses.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSolutions.Problems.Problem0020_ValidParentheses;
+
+public static class Problem0020_ValidParentheses
+{
+    public static bool IsValid(string s)
+    {
+        if (s.Length < 2 || s[0] == ')' || s[0] == ']' || s[0] == '}')
+        {
+            return false;
+        }
+
+        Stack<int> pila = new();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                pila.Push(s[i]);
+            }
+            else if (s[i] == ')' && pila.Count > 0 && pila.Peek() == '(')
+            {
+                pila.Pop();
+            }
+            else if (s[i] == ']' && pila.Count > 0 && pila.Peek() == '[')
+            {
+                pila.Pop();
+            }
+            else if (s[i] == '}' && pila.Count > 0 && pila.Peek() == '{')
+            {
+                pila.Pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return pila.Count == 0;
+    }
+}

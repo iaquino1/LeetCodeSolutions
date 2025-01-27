cc4f1b7086d5e8aee5dc04530b4162da4cac9ca4
Luis Enrique Aquino Cortés <aquino.lec@gmail.com>
2025-01-26 19:59:42 -0600

    Problema 21 Merge Two Sorted Lists.
    Se agrega solución y prueba unitaria del problema 21 Merge Two Sorted Lists.

---
+++ LeetCodeSolutions.Tests/Problem0021Tests.cs
@@ -0,0 +1,51 @@
+﻿using LeetCodeSolutions.Problems.Problem0021_MergeTwoSortedLists;
+using LeetCodeSolutions.Shared;
+using NUnit.Framework;
+
+namespace LeetCodeSolutions.Tests;
+
+[TestFixture]
+public class Problem0021Tests
+{
+    private static IEnumerable<TestCaseData> MergeTwoSortedListsTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new ListNode(1, new ListNode(2, new ListNode(4))),
+                new ListNode(1, new ListNode(3, new ListNode(4))),
+                new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4)))))));
+            yield return new TestCaseData(
+                null,
+                null,
+                null);
+            yield return new TestCaseData(
+                null,
+                new ListNode(0),
+                new ListNode(0));
+        }
+    }
+
+    [Test, TestCaseSource(nameof(MergeTwoSortedListsTestCases))]
+    public void MergeTwoLists_ShouldReturnExpectedResult(ListNode list1, ListNode list2, ListNode expected)
+    {
+        // Act
+        ListNode result = Problem0021_MergeTwoSortedLists.MergeTwoLists(list1, list2);
+
+        // Assert
+        //Assert.That(result, Is.EqualTo(expected));
+        Assert.That(AreListsEqual(result, expected));
+    }
+
+    private static bool AreListsEqual(ListNode a, ListNode b)
+    {
+        while (a != null && b != null)
+        {
+            if (a.val != b.val) return false;
+            a = a.next;
+            b = b.next;
+        }
+
+        return a == null && b == null;
+    }
+}
---
+++ LeetCodeSolutions/Problems/Problem0021_MergeTwoSortedLists/Problem0021_MergeTwoSortedLists.cs
@@ -0,0 +1,73 @@
+﻿using LeetCodeSolutions.Shared;
+
+namespace LeetCodeSolutions.Problems.Problem0021_MergeTwoSortedLists;
+
+public static class Problem0021_MergeTwoSortedLists
+{
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode cabecera = null;
+        ListNode nodoActual;
+
+        if (list1 != null && list2 != null)
+        {
+            if (list1.val >= list2.val)
+            {
+                cabecera = new ListNode(list2.val, list1);
+                list2 = list2.next;
+            }
+            else
+            {
+                cabecera = new ListNode(list1.val, list2);
+                list1 = list1.next;
+            }
+        }
+        else if (list1 != null)
+        {
+            cabecera = list1;
+            list1 = list1.next;
+        }
+        else if (list2 != null)
+        {
+            cabecera = list2;
+            list2 = list2.next;
+        }
+        else
+        {
+            return cabecera;
+        }
+
+        nodoActual = cabecera;
+
+        while (list1 != null || list2 != null)
+        {
+            if (list1 != null && list2 != null)
+            {
+                if (list1.val >= list2.val)
+                {
+                    nodoActual.next = new ListNode(list2.val, list1);
+                    list2 = list2.next;
+                }
+                else
+                {
+                    nodoActual.next = new ListNode(list1.val, list2);
+                    list1 = list1.next;
+                }
+            }
+            else if (list1 != null)
+            {
+                nodoActual.next = list1;
+                list1 = list1.next;
+            }
+            else if (list2 != null)
+            {
+                nodoActual.next = list2;
+                list2 = list2.next;
+            }
+
+            nodoActual = nodoActual.next;
+        }
+
+        return cabecera;
+    }
+}
